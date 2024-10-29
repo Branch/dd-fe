559d1dea412a81eea6a1e0d009a75e01f8f8d3bf
Andreas Beuger <andreasbeuger@gmail.com>
2024-10-29 22:25:37 +0100

    feat: isexternalurl check use different env variable

--- src/utils/isExternalUrl.ts
+++ src/utils/isExternalUrl.ts
@@ -1,7 +1,5 @@
 const isExternalUrl = (url: string) => {
-  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL
-    ? process.env.NEXT_PUBLIC_SITE_URL
-    : "";
+  const siteUrl = process.env.BASE_URL ? process.env.BASE_URL : "";
 
   // If site url env match or missing protocol, it's an internal url
   return url.includes(siteUrl) || !url.includes("://") ? false : true;
