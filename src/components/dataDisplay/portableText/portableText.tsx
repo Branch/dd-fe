8e4ba5d54c3d5607f3c1e95c83a3901324bbea9e
Andreas Beuger <andreasbeuger@gmail.com>
2025-02-21 20:30:35 +0100

    styles: button my

--- src/components/dataDisplay/portableText/portableText.tsx
+++ src/components/dataDisplay/portableText/portableText.tsx
@@ -120,7 +120,7 @@ const PageComponents = ({
           <a
             href={value.url}
             target="_blank"
-            className={`${style === "primary" ? "bg-djungleGreen !text-white hover:bg-djungleGreen/90" : "!bg-transparent border-2 border-djungleGreen !text-djungleGreen hover:!bg-djungleGreen-50"} flex !no-underline items-center justify-center gap-2 text-xl px-4 py-2 rounded-md mt-4 w-full md:w-1/2 text-center mx-auto font-bold`}
+            className={`${style === "primary" ? "bg-djungleGreen !text-white hover:bg-djungleGreen/90" : "!bg-transparent border-2 border-djungleGreen !text-djungleGreen hover:!bg-djungleGreen-50"} flex !no-underline items-center justify-center gap-2 text-xl px-4 py-2 rounded-md my-4 w-full md:w-1/2 text-center mx-auto font-bold`}
             rel={`noopener noreferrer nofollow${isSponsored ? " sponsored" : ""}`}
           >
             {value.text}
