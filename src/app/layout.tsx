0645925cbd15c1717fb45727b671b5e5b0c22733
Andreas Beuger <andreasbeuger@gmail.com>
2025-03-08 12:34:49 +0100

    compose header if featch successful

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,30 +21,33 @@ export default async function RootLayout({
     `${process.env.BASE_URL}/api/navigation/header`
   );
   const nav = await navQ?.json();
-  const navSections = await Promise.all(
-    nav.sections.map(async (s: any) => {
-      const d = await tryCatchFetch(
-        `${process.env.BASE_URL}/api/page/metaData/id/${s.target._id}`
-      );
-      const data = await d?.json();
-      return {
-        ...s,
-        fullPath: data?.path,
-        links: await Promise.all(
-          s?.links?.map(async (l: any) => {
-            const d = await tryCatchFetch(
-              `${process.env.BASE_URL}/api/page/metaData/id/${l.target._id}`
-            );
-            const data = await d?.json();
-            return {
-              ...l,
-              fullPath: data?.path,
-            };
-          }) || []
-        ),
-      };
-    })
-  );
+  let navSections = [];
+  if (navQ) {
+    navSections = await Promise?.all(
+      nav?.sections?.map(async (s: any) => {
+        const d = await tryCatchFetch(
+          `${process.env.BASE_URL}/api/page/metaData/id/${s.target._id}`
+        );
+        const data = await d?.json();
+        return {
+          ...s,
+          fullPath: data?.path,
+          links: await Promise.all(
+            s?.links?.map(async (l: any) => {
+              const d = await tryCatchFetch(
+                `${process.env.BASE_URL}/api/page/metaData/id/${l.target._id}`
+              );
+              const data = await d?.json();
+              return {
+                ...l,
+                fullPath: data?.path,
+              };
+            }) || []
+          ),
+        };
+      })
+    );
+  }
   return (
     <html lang="sv-SE">
       <GoogleTagManager gtmId="GTM-KGXK3JMC" />
