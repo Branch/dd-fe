ed492d56f32cf9628b0dbb582fd8ffd3109bf199
Andreas Beuger <andreasbeuger@gmail.com>
2025-03-16 09:33:47 +0100

    next cfg sitemap nocache

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,19 @@
  */
 
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     deviceSizes: [640, 750, 828],
     remotePatterns: [
