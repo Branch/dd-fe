2c8281b65568bdc7fa650835bb026cb1f1773ac9
Andreas Beuger <andreasbeuger@gmail.com>
2025-03-17 13:12:55 +0100

    fix: button isexternal check

--- src/components/dataDisplay/portableText/portableText.tsx
+++ src/components/dataDisplay/portableText/portableText.tsx
@@ -122,7 +122,7 @@ const PageComponents = ({
       button: ({ value }) => {
         const isSponsored = value.isSponsored;
         const style = value.style;
-        return isExternalUrl(value.url) ? (
+        return !isExternalUrl(value.url) ? (
           <Link
             className={`${style === "primary" ? "bg-djungleGreen !text-white hover:bg-djungleGreen/90" : "!bg-transparent border-2 border-djungleGreen !text-djungleGreen hover:!bg-djungleGreen-50"} flex !no-underline items-center justify-center gap-2 text-xl px-4 py-2 rounded-md my-4 w-full md:w-1/2 text-center mx-auto font-bold`}
             href={value.url}
